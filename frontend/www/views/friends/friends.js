692772372e16c5af7e77cd97ec7260b9d703256b
dduvivie-zz <dduvivie@shi-r7-p5.s19.be>
2024-01-05 13:36:10 +0100

    fix friend's profile redirection

--- frontend/www/views/friends/friends.js
+++ frontend/www/views/friends/friends.js
@@ -1,41 +1,3 @@
-// const url = backendUrl + "/friends/friends_view/";
-
-// fetch(url)
-//     .then(response => response.json())
-//     .then(data => {
-//         const friendsContainer = document.getElementById('friends-container');
-//         data.friends.forEach(friend => {
-//             const friendElement = createFriendElement(friend);
-//             friendsContainer.appendChild(friendElement);
-//         });
-//     });
-
-// function createFriendElement(friend) {
-//     const friendElement = document.createElement('div');
-//     friendElement.classList.add(friend.online_status ? 'online-status-true' : 'online-status-false');
-//     friendElement.innerHTML = `
-//         <p>${friend.username} - <span class="status">${friend.online_status ? 'Online' : 'Offline'}</span></p>
-//         <button class="add-button">ADD</button>
-//         <hr>
-//     `;
-
-//     friendElement.querySelector('.add-button').addEventListener('click', () => {
-//         addFriend(friend.username);
-//     });
-//     return friendElement;
-// }
-
-// function addFriend(username) {
-//     console.log(`Adding friend: ${username}`);
-// }
-
-//<button class="add-button" onclick="addFriend('${friend.username}')">ADD</button>
-
-
-//-------------------------------------------------------------------------------------------------------------------------//
-
-
-
 /*
 
     Function called to redirect on the user page when someone click on a username 
@@ -103,7 +65,7 @@ function set_friend_list() {
             });
 
             friendElement.querySelector('.other_user').addEventListener('click', () => {
-                redirect_other_profile(user.username);
+                redirect_other_profile(friend.username);
             });
 
             return friendElement;
